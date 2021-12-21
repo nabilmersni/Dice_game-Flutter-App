a8885ba7948c87eff7b3c96e11f4c868fc68715a
nabilmersni <nabilmersni123@gmail.com>
2021-12-21 15:06:35 +0100

    finishing the app ui

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,51 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Center(
+              child: Text('Dice'),
+            ),
+            backgroundColor: Color(0xff293241),
+          ),
+          body: GameBody(),
+          backgroundColor: Color(0xff3d5a80),
+        ),
+      ),
+    );
+  }
+}
+
+class GameBody extends StatelessWidget {
+  const GameBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+            SizedBox(
+              width: 20,
+            ),
+            Expanded(
+              flex: 1,
+              child: Image(
+                image: AssetImage('images/dice1.png'),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
