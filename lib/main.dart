4ec9b95fb1e63e86282f47f18a44c1921ba6778c
nabilmersni <nabilmersni123@gmail.com>
2021-12-22 11:41:30 +0100

    finishing the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -27,35 +28,85 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class GameBody extends StatelessWidget {
+class GameBody extends StatefulWidget {
   const GameBody({Key? key}) : super(key: key);
 
   @override
+  _GameBodyState createState() => _GameBodyState();
+}
+
+class _GameBodyState extends State<GameBody> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Padding(
-        padding: const EdgeInsets.all(15),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Expanded(
-              flex: 1,
-              child: Image(
-                image: AssetImage('images/dice1.png'),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                flex: 1,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = randomNumberFN();
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage('images/dice$leftDiceNumber.png'),
+                  ),
+                ),
               ),
-            ),
-            SizedBox(
-              width: 20,
-            ),
-            Expanded(
-              flex: 1,
-              child: Image(
-                image: AssetImage('images/dice1.png'),
+              const SizedBox(
+                width: 20,
               ),
-            ),
-          ],
+              Expanded(
+                flex: 1,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      rightDiceNumber = randomNumberFN();
+                    });
+                  },
+                  child: Image(
+                    image: AssetImage('images/dice$rightDiceNumber.png'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
+        const SizedBox(
+          height: 70,
+        ),
+        ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            padding: EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+            primary: Color(0xffFF7244),
+          ),
+          onPressed: () {
+            setState(() {
+              leftDiceNumber = 1;
+              rightDiceNumber = 1;
+            });
+          },
+          child: const Text(
+            'Reset',
+            style: TextStyle(
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ],
     );
   }
+}
+
+int randomNumberFN() {
+  int randomNumber = Random().nextInt(6) + 1;
+  return randomNumber;
 }
